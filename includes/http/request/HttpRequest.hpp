359b34d434b6538c13ce716cc1fee8aa57417e89
paprika_iuan <joangirbau96@gmail.com>
2026-07-05 16:58:46 +0200

    Method part done. Headers and body to be done.

--- includes/http/request/HttpRequest.hpp
+++ includes/http/request/HttpRequest.hpp
@@ -4,64 +4,50 @@
 #include <map>
 #include <vector>
 
-typedef struct {
-	std::string methodHeader;
-	t_uri		uri;
-	std::string	HttpVer;
-}	t_method;
-
 class Request {
 	private:
-		HandleSocket	_socket;
-		char			*_rawRequest;
-		t_method		_method;
+		std::string _method;
+		t_uri		_uri;
+		std::string	_httpVer;
+		
 		std::map<std::string, std::string>  _headers;
 		std::vector<char>	_body;
+		
+		bool _methodParsed;
+		bool _uriParsed;
+		bool _httpVerParsed;
+		
+		bool _headRead;
+		bool _bodyRead;
+
+		std::string _uriStr;
+		std::string _leftover;
+		
 
 		//Functs
 		//void recieveRawRequest();
-		void parseRequest();
+		bool parseRequestHead(char *stream);
+		bool parseRequestBody(char *stream);
 		// Will call this functs staticlly.
 		// Since they cannot be called independently if've chosen this way.
-		// - static void parseMethod();
+		bool parseMethod(std::stringstream &ss)
 		// - static void parseHeaders();
 		// - static void parseBody();
 
 	public:
 		Request();
-		Request(const HandleSocket &socket);
 		Request(const Request &other);
 		Request &operator=(const Request &other);
 		~Request();
 
 	//Getters
-		HandleSocket	getSocket;
+		
 		t_method		getMethod;
 		std::map<std::string, std::string>  getHeaders;
 		std::vector<char>	getBody;
 };
 
 ///////////////////////////////////////////////////////
-// This will go to another file. 
-
-enum class State {
-	READING_HEADERS;
-	READING_BODY;
-	PROCESSING;
-	WRITING_RESPONSE;
-	FINISHED;
-};
-
-class Http {
-	private:
-		State		state;
-		Request		&req;
-		// Response	&res;
-
-	public:
-		//canonish;
-
-}
 
 // Acaba de decidir com fas el http general
 // Acaba de dissenyar el flow de la maquina d'estats.
--- src/http/request/HttpRequest.cpp
+++ src/http/request/HttpRequest.cpp
@@ -1,58 +1,99 @@
 #include "HttpRequest.hpp"
 
-Request::Request()
-
-Request(const HandleSocket &sourceSocket) : _socket(sourceSocket) {}
-
+Request::Request() : _headRead(false), _bodyRead(false) {}
 
 Request::Request(const Request &other) {
-    this = other;
+	this = other;
 }
 
-Request::Request &operator=(const Request &other) {
-    if (this != &other) {
-        this->_rawRequest = other._rawRequest;
-        this->_methodHeader = other._methodHeader;
-        this->_headers = other._headers;
-        this->_body = other._body;
-    }
-    return *this;
+Request &Request::operator=(const Request &other) {
+	if (this != &other) {
+		this->_rawRequest = other._rawRequest;
+		this->_methodHeader = other._methodHeader;
+		this->_headers = other._headers;
+		this->_body = other._body;
+	}
+	return *this;
 }
 
 Request::~Request() {}
 
-//Functs
-//void Request::recieveRawRequest();
-// -- He de parlar am el Isaac a veure com em passa la info/com la he danar a buscar. 
+// Functs
 
-
-void Request::parseRequest() {
-    char buff[1024];
-    while (1) {
-        int  = recv(this.socket.getFD(), &buff, 1024, MSG_DONTWAIT);
-    }
+static void safeGetLine(std::stringstream &ss, std::string &leftover, char delimiter, bool &done) {
+	std::string line;
+	if (!std:;getline(ss, line, delimiter)) {
+		if (ss.fail() || ss.bad())
+			// 400 Bad Request.
+		leftover += line;
+		return;
+	}
+	leftover += line;
+	done = true;
 }
 
-// Will call this functs staticlly.
-// Since they cannot be called independently if've chosen this way.
-static void parseMethod();
-static void parseHeaders();
+bool Request::parseMethod(std::stringstream &ss) {
+	if (!this._methodParsed) {
+		safeGetLine(ss, _method, ' ', this._methodParsed);
+		if (!this._methodParsed)
+			return 0;
+		if (methodStr != "GET" && methodStr != "POST" && methodStr != "DELETE") // To be redone by server restrictions.
+			// 400 Bad Request.
+	}
+
+
+	if (!this._uriParsed) {
+		safeGetLine(ss, _uriStr, ' ', this._uriParsed);
+		if (!this._uriParsed)
+			return 0;
+		this._uri = uriParser(_uriStr);
+		this._uriStr.clear();
+	}
+
+	if (!this._httpVerParsed) {
+		safeGetLine(ss, _httpVer, '\r', this._httpVerParsed);
+		if (!this._httpVerParsed)
+			return 0;
+		if (httpVerStr != "HTTP/1.1")
+			// 400 Bad Request.
+		char peek = ss.peek();
+		if (peek == '\n')
+			ss.ignore();
+		else
+			// 400 Bad Request.
+	}
+	return true;
+}
+
+static void parseHeaders() {
+
+}
+
 static void parseBody();
+
+bool Request::parseRequestHead(char *stream) {
+	std::stringstream ss(stream);
+	if (!this.parseMethod(ss))
+		return false;
+	this.parseHeaders(ss);
+	return true;
+}
+
+bool Request::parseRequestBody(char *stream);
+
+
+
 
 
 //Getters
-t_method	Request::getSocket {
-    return this->_socket;
-}
-
 t_method	Request::getMethod {
-    return this->_method;
+	return this->_method;
 }
 
 std::map<std::string, std::string>  Request::getHeaders {
-    return this->_headers;
+	return this->_headers;
 }
 
 std::vector<char>	Request::getBody {
-    return this->body;
+	return this->body;
 }
